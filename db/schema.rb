0bd37b486732fdf375e17ae64057aecec0e7d571
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-11-05 19:12:20 -0600

    Remove Devise dependant columns from Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615124420) do
+ActiveRecord::Schema.define(version: 20161106010847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,15 +165,6 @@ ActiveRecord::Schema.define(version: 20160615124420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.boolean  "agent",                  default: false, null: false
     t.text     "signature"
     t.boolean  "notify",                 default: true
@@ -184,9 +175,9 @@ ActiveRecord::Schema.define(version: 20160615124420) do
     t.boolean  "prefer_plain_text",      default: false, null: false
     t.boolean  "include_quote_in_reply", default: true,  null: false
     t.string   "name"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
