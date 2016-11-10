bad3b04d46eaa575dc376c88cd891f0ad9d213e6
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-11-09 22:12:25 -0700

    Add slack service test

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -1,20 +1,17 @@
 class SlackService
 
+  attr_reader :slack_code
+
   def initialize(slack_code)
     @slack_code = slack_code
   end
 
   def fetch_user_info_from_slack
-    slack = Slack.oauth_access({:client_id => ENV['SLACK_APP_ID'], :client_secret => ENV['SLACK_APP_SECRET'], :code => @slack_code})
-    byebug
+    Slack.oauth_access({:client_id => ENV['SLACK_APP_ID'], :client_secret => ENV['SLACK_APP_SECRET'], :code => @slack_code})
   end
 
   def slack_user_attrs
     SlackUser.new(fetch_user_info_from_slack(slack_code))
   end
-
-  private
-
-  attr_reader :slack_code
-
+  
 end
